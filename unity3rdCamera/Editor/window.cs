be8b8b56748ec6a86c815bf4f20e08c90892593d
caidao <453127577@qq.com>
2018-01-15 18:20:55 +0800

    qqq
    第三人称摄像机实验

---
+++ unity3rdCamera/Editor/window.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class MyWindow : EditorWindow
+{
+	string myString = "Hello World";
+	bool groupEnabled;
+	bool myBool = true;
+	float myFloat = 1.23f;
+
+	// Add menu named "My Window" to the Window menu
+	[MenuItem("Window/My Window")]
+	static void Init()
+	{
+		// Get existing open window or if none, make a new one:
+		MyWindow window = (MyWindow)EditorWindow.GetWindow(typeof(MyWindow));
+		window.Show();
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+		myString = EditorGUILayout.TextField("Text Field", myString);
+
+		groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+		myBool = EditorGUILayout.Toggle("Toggle", myBool);
+		myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+		GUILayout.Label("Base Settings1", EditorStyles.boldLabel);
+		EditorGUILayout.Vector3Field ("aaa", new Vector3 (1, 2, 3));
+		EditorGUILayout.EndToggleGroup();
+	}
+}
---
+++ unity3rdCamera/WaypointPath.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Note: this is not an editor script.
+public class WaypointPath : MonoBehaviour {
+	public Transform[] wayPointArray = new Transform[1];
+
+	void awek()
+	{
+		wayPointArray [1] = GameObject.Find ("Cube").transform;
+	}
+
+}
